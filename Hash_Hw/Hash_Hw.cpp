2ba372143eeca313f0e079d70692e2874f61e6cf
jkuczynski1 <jkuczynski1@luc.edu>
2016-04-25 23:23:44 -0500

    testing...

--- Hash_Hw/Hash_Hw.cpp
+++ Hash_Hw/Hash_Hw.cpp
@@ -1,11 +1,7 @@
 // Hash_Hw.cpp : Defines the entry point for the console application.
 //
-//
-//Supposed to ceate a hash table based on the extraction method with collision prevention enabled.
 
 #include "stdafx.h"
-#include <iostream>
-using namespace std;
 
 
 int main()
